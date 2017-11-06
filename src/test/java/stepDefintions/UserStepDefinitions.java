cccc9fa8393cfe3979f67efef4bc1afd0435ef2e
Tomas Burleigh <t.burleigh@gmail.com>
2017-11-07 09:58:57 +1300

    Updated guava and selenium

--- src/test/java/stepDefintions/UserStepDefinitions.java
+++ src/test/java/stepDefintions/UserStepDefinitions.java
@@ -1,6 +1,11 @@
 package stepDefintions;
+import cucumber.api.java.After;
 import env.BaseTest;
+import env.DriverUtil;
 
 public class UserStepDefinitions implements BaseTest {
-
+	@After
+	public void after() {
+		DriverUtil.closeDriver();
+	}
 }
